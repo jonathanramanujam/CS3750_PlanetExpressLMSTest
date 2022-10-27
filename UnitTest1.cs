046349335725d76b8c3f1d27c579ae6ed2a2f297
19smonkres <sfmonkres@gmail.com>
2022-10-26 21:34:58 -0600

    test works thanks bestie jon

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -20,33 +20,35 @@ namespace CS3750_PlanetExpressLMSTest
         {
             DbContextOptions<CS3750_PlanetExpressLMSContext> options = new DbContextOptions<CS3750_PlanetExpressLMSContext>();
             DbContextOptionsBuilder builder = new DbContextOptionsBuilder(options);
-            SqlServerDbContextOptionsExtensions.UseSqlServer(builder, "Data Source=titan.cs.weber.edu,10433;Initial Catalog=LMS_Planet;Persist Security Info=True;User ID=LMS_Planet;Password=PlanetExpress!!", null);
+            SqlServerDbContextOptionsExtensions.UseSqlServer(builder, "Data Source=titan.cs.weber.edu,10433;Initial Catalog=LMS_Planet;Persist Security Info=True;User ID=LMS_Planet;Password=Planetexpress!!");
             var _context = new CS3750_PlanetExpressLMSContext((DbContextOptions<CS3750_PlanetExpressLMSContext>)builder.Options);
 
             //setup: login
             LoginModel login = new LoginModel(null) { user = _context.User.FirstOrDefault(u => u.Email == "professor25@email.com")};
-            //int n = _context.Course.Count();
+            int n = _context.Course.Count();
 
-            // Make sure a user is logged in
-            //login.OnGet();
-            //login.user = _context.User.FirstOrDefault(u => u.Email == "professor25@email.com");
-            /*login.user.Email = "faketeacher@mail.com";
-            login.user.Password = "password";*/
-            login.OnPostAsync();
+            SQLCourseRepository courseRepository = new SQLCourseRepository(_context);
+            Course newCourse = new Course();
+            newCourse.CreditHours = 4;
+            newCourse.CourseNumber = 1234;
+            newCourse.CourseName = "Test Course";
+            newCourse.Department = "CD";
+            newCourse.StartDate = DateTime.Now;
+            newCourse.EndDate = new DateTime(2023, 12, 25);
+            DateTime startTime = DateTime.Parse("10/22/2022 07:00:00 AM");
+            startTime.ToString("HH:mm");
+            newCourse.StartTime = startTime;
+            DateTime endTime = DateTime.Parse("10/22/2022 05:00:00 PM");
+            endTime.ToString("HH:mm");
+            newCourse.EndTime = endTime;
+            newCourse.UserID = 1;
+            newCourse.CourseLocation = "Nowhere";
+            newCourse.Days = "Sun";
 
-            CoursesModel courseMod = new CoursesModel(null, null);
-            courseMod.OnGetAsync();
-            courseMod.course.CourseNumber = 1234;
-            courseMod.course.CourseName = "Test Course";
-            courseMod.course.CourseLocation = "Nowhere";
-            courseMod.course.StartDate = DateTime.Now;
-            courseMod.course.EndDate = new DateTime(2023, 12, 25);
-            courseMod.course.Days = "0000001"; 
-
-            courseMod.OnPostAsync();
+            courseRepository.Add(newCourse);
 
             int m = _context.Course.Count();
-            //Assert.IsTrue(m == n + 1);
+            Assert.IsTrue(m == n + 1);
         }
     }
 }
