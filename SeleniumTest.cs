746ae3f110e75b3a7976c99326e157281779e550
Jonathan Ramanujam <75343684+jonathanramanujam@users.noreply.github.com>
2022-10-31 22:13:07 -0600

    Firefox Working

--- SeleniumTest.cs
+++ SeleniumTest.cs
@@ -13,50 +13,10 @@ using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Edge;
 using OpenQA.Selenium;
 using WebDriverManager.Helpers;
+using System.Linq.Expressions;
 
 namespace CS3750_PlanetExpressLMSTest
 {
-    [TestClass]
-    public class FirstScriptTest
-    {
-
-        protected IWebDriver driver;
-
-        [TestInitialize]
-        public void CreateDriver()
-        {
-            new DriverManager().SetUpDriver(new ChromeConfig());
-            driver = new ChromeDriver();
-        }
-
-        [TestCleanup]
-        public void QuitDriver()
-        {
-            driver.Quit();
-        }
-
-        [TestMethod]
-        public void ChromeSession()
-        {
-            driver.Navigate().GoToUrl("https://www.selenium.dev/selenium/web/web-form.html");
-
-            var title = driver.Title;
-            Assert.AreEqual("Web form", title);
-
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(500);
-
-            var textBox = driver.FindElement(By.Name("my-text"));
-            var submitButton = driver.FindElement(By.TagName("button"));
-
-            textBox.SendKeys("Selenium");
-            submitButton.Click();
-
-            var message = driver.FindElement(By.Id("message"));
-            var value = message.Text;
-            Assert.AreEqual("Received!", value);
-        }
-    }
-
     [TestClass]
     public class SeleniumChrome
     {
@@ -120,8 +80,19 @@ namespace CS3750_PlanetExpressLMSTest
         [TestMethod]
         public void FirefoxLogin()
         {
-            driver.Navigate().GoToUrl("https://localhost:44390/login");
+            try
+            {
+                driver.Navigate().GoToUrl("https://localhost:44390/login");
+            }
+            catch
+            {
+                var buttonAdvanced = driver.FindElement(By.Id("advancedButton"));
+                buttonAdvanced.Click();
 
+                var buttonException = driver.FindElement(By.Id("exceptionDialogButton"));
+                buttonException.Click();
+            }         
+            
             var title = driver.Title;
             Assert.AreEqual("Login - Planet Express Learning Management System", title);
 
