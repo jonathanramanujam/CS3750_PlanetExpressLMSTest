22ba9279a1c420a6cb7f076e4d5e776a4cc4b9c4
Nathan Blair <nathanblair@mail.weber.edu>
2022-11-09 21:11:14 -0700

    Firefox and Edge Test
    Added Firefox and edge

--- SeleniumTest.cs
+++ SeleniumTest.cs
@@ -149,6 +149,64 @@ namespace CS3750_PlanetExpressLMSTest
             var value = message.Text;
             Assert.AreEqual("Dashboard", value);
         }
+
+        [TestMethod]
+        public void FirefoxMakePayment()
+        {
+            try
+            {
+                driver.Navigate().GoToUrl("https://localhost:44390/login");
+            }
+            catch
+            {
+                var buttonAdvanced = driver.FindElement(By.Id("advancedButton"));
+                buttonAdvanced.Click();
+
+                var buttonException = driver.FindElement(By.Id("exceptionDialogButton"));
+                buttonException.Click();
+            }
+
+            var title = driver.Title;
+            Assert.AreEqual("Login - Planet Express Learning Management System", title);
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(500);
+
+            var textboxUsername = driver.FindElement(By.Id("textbox-username"));
+            var textboxPassword = driver.FindElement(By.Id("textbox-password"));
+            var buttonLogin = driver.FindElement(By.Id("button-login"));
+
+            textboxUsername.SendKeys("test@test.com");
+            textboxPassword.SendKeys("password");
+            buttonLogin.Click();
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+            IWebElement element = wait.Until(ExpectedConditions.ElementIsVisible(By.Id("page-title")));
+
+            /*            var message = driver.FindElement(By.Id("page-title"));
+                        var value = message.Text;
+                        Assert.AreEqual("Dashboard", value);*/
+
+            var paymentLink = driver.FindElement(By.LinkText("Account"));
+            paymentLink.Click();
+
+            element = wait.Until(ExpectedConditions.ElementIsVisible(By.Id("page-title")));
+
+            var textboxFirstName = driver.FindElement(By.Name("txtFirstName"));
+            var textboxLastName = driver.FindElement(By.Name("txtLastName"));
+            var textboxCardNumber = driver.FindElement(By.Name("txtCardNumber"));
+            var textboxExpDate = driver.FindElement(By.Name("txtExpDate"));
+            var textboxCvv = driver.FindElement(By.Name("txtCvv"));
+            var textboxAmount = driver.FindElement(By.Name("txtAmount"));
+            var submitButton = driver.FindElement(By.ClassName("btn btn-primary"));
+
+            textboxFirstName.SendKeys("test");
+            textboxLastName.SendKeys("test");
+            textboxCardNumber.SendKeys("4242424242424242");
+            textboxCvv.SendKeys("123");
+            textboxExpDate.SendKeys("December2023");
+            textboxAmount.SendKeys("5");
+            submitButton.Click();
+        }
     }
 
     [TestClass]
@@ -190,6 +248,48 @@ namespace CS3750_PlanetExpressLMSTest
             var message = driver.FindElement(By.Id("page-title"));
             var value = message.Text;
             Assert.AreEqual("Dashboard", value);
+        }
+
+        [TestMethod]
+        public void EdgeMakePayment()
+        {
+            driver.Navigate().GoToUrl("https://localhost:44390/login");
+
+            var title = driver.Title;
+            Assert.AreEqual("Login - Planet Express Learning Management System", title);
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(500);
+
+            var textboxUsername = driver.FindElement(By.Id("textbox-username"));
+            var textboxPassword = driver.FindElement(By.Id("textbox-password"));
+            var buttonLogin = driver.FindElement(By.Id("button-login"));
+
+            textboxUsername.SendKeys("test@test.com");
+            textboxPassword.SendKeys("password");
+            buttonLogin.Click();
+
+            /*            var message = driver.FindElement(By.Id("page-title"));
+                        var value = message.Text;
+                        Assert.AreEqual("Dashboard", value);*/
+
+            var paymentLink = driver.FindElement(By.LinkText("Account"));
+            paymentLink.Click();
+
+            var textboxFirstName = driver.FindElement(By.Name("txtFirstName"));
+            var textboxLastName = driver.FindElement(By.Name("txtLastName"));
+            var textboxCardNumber = driver.FindElement(By.Name("txtCardNumber"));
+            var textboxExpDate = driver.FindElement(By.Name("txtExpDate"));
+            var textboxCvv = driver.FindElement(By.Name("txtCvv"));
+            var textboxAmount = driver.FindElement(By.Name("txtAmount"));
+            var submitButton = driver.FindElement(By.ClassName("btn btn-primary"));
+
+            textboxFirstName.SendKeys("test");
+            textboxLastName.SendKeys("test");
+            textboxCardNumber.SendKeys("4242424242424242");
+            textboxCvv.SendKeys("123");
+            textboxExpDate.SendKeys("December2023");
+            textboxAmount.SendKeys("5");
+            submitButton.Click();
         }
     }
 }
