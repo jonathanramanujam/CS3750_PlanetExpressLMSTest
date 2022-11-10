eaea4e2b46fa2e80a40cf5581057801f9dfe65c8
Nathan Blair <nathanblair@mail.weber.edu>
2022-11-09 18:07:19 -0700

    Payment Unit Test
    Can add payment

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -160,5 +160,32 @@ namespace CS3750_PlanetExpressLMSTest
             int m = _context.Enrollment.Count();
             Assert.IsTrue(m == n - 1);
         }
+
+        [TestMethod]
+        public void canCreatePayment()
+        {
+            DbContextOptions<CS3750_PlanetExpressLMSContext> options = new DbContextOptions<CS3750_PlanetExpressLMSContext>();
+            DbContextOptionsBuilder builder = new DbContextOptionsBuilder(options);
+            SqlServerDbContextOptionsExtensions.UseSqlServer(builder, "Data Source=titan.cs.weber.edu,10433;Initial Catalog=LMS_Planet;Persist Security Info=True;User ID=LMS_Planet;Password=Planetexpress!!");
+            var _context = new CS3750_PlanetExpressLMSContext((DbContextOptions<CS3750_PlanetExpressLMSContext>)builder.Options);
+
+            //setup: login
+            LoginModel login = new LoginModel(null) { user = _context.User.FirstOrDefault(u => u.Email == "studentstudent2@mail.com") };
+            int n = _context.Payment.Count();
+
+            SQLPaymentRepository paymentRepo = new SQLPaymentRepository(_context);
+            Payment payAdd = new Payment();
+            payAdd.FirstName = "Test";
+            payAdd.LastName = "Test";
+            payAdd.CardNumber = "4242424242424242";
+            payAdd.Cvv = "123";
+            payAdd.ExpDate = new DateTime(2023, 12, 21, 23, 59, 00);
+            payAdd.ID = login.user.ID;
+
+            paymentRepo.Add(payAdd);
+            //if there is no test course to delete, this will fail, just run createCourse before it
+            int m = _context.Payment.Count();
+            Assert.IsTrue(m == n + 1);
+        }
     }
 }
