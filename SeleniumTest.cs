a7adf36bf9488b2141e66a4f18b798198f8e5055
Nathan Blair <nathanblair@mail.weber.edu>
2022-11-09 22:06:01 -0700

    arrow key
    arrow key right for exp date

--- SeleniumTest.cs
+++ SeleniumTest.cs
@@ -91,7 +91,7 @@ namespace CS3750_PlanetExpressLMSTest
             textboxLastName.SendKeys("test");
             textboxCardNumber.SendKeys("4242424242424242");
             textboxCvv.SendKeys("123");
-            textboxExpDate.SendKeys("December2023");
+            textboxExpDate.SendKeys("December" + Keys.ArrowRight + "2023");
             textboxAmount.SendKeys("5");
             submitButton.Click();
 
@@ -210,7 +210,7 @@ namespace CS3750_PlanetExpressLMSTest
             textboxLastName.SendKeys("test");
             textboxCardNumber.SendKeys("4242424242424242");
             textboxCvv.SendKeys("123");
-            textboxExpDate.SendKeys("December2023");
+            textboxExpDate.SendKeys("December" + Keys.ArrowRight + "2023");
             textboxAmount.SendKeys("5");
             submitButton.Click();
 
@@ -285,7 +285,7 @@ namespace CS3750_PlanetExpressLMSTest
             var paymentLink = driver.FindElement(By.LinkText("Account"));
             paymentLink.Click();
 
-            var originalBalance = driver.FindElement(By.Id("balance"));
+            var originalBalance = driver.FindElement(By.Id("balance")).Text;
 
             var textboxFirstName = driver.FindElement(By.Name("txtFirstName"));
             var textboxLastName = driver.FindElement(By.Name("txtLastName"));
@@ -299,12 +299,12 @@ namespace CS3750_PlanetExpressLMSTest
             textboxLastName.SendKeys("test");
             textboxCardNumber.SendKeys("4242424242424242");
             textboxCvv.SendKeys("123");
-            textboxExpDate.SendKeys("December2023");
+            textboxExpDate.SendKeys("December" + Keys.ArrowRight + "2023");
             textboxAmount.SendKeys("5");
             submitButton.Click();
 
-            var newBalance = driver.FindElement(By.Id("balance"));
-            Assert.AreNotEqual(originalBalance.Text, newBalance.Text);
+            var newBalance = driver.FindElement(By.Id("balance")).Text;
+            Assert.AreNotEqual(originalBalance, newBalance);
         }
     }
 }
