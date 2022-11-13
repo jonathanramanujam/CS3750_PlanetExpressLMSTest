f73c11b1749ac830388095143c6f2c6ec075b0c8
19smonkres <sfmonkres@gmail.com>
2022-11-12 19:06:33 -0700

    fixed some stuff

--- SeleniumTest.cs
+++ SeleniumTest.cs
@@ -110,7 +110,7 @@ namespace CS3750_PlanetExpressLMSTest
             var textboxPassword = driver.FindElement(By.Id("textbox-password"));
             var buttonLogin = driver.FindElement(By.Id("button-login"));
 
-            textboxUsername.SendKeys("fakestudent@mail.com");
+            textboxUsername.SendKeys("faketeacher@mail.com");
             textboxPassword.SendKeys("password");
             buttonLogin.Click();
 
@@ -121,7 +121,7 @@ namespace CS3750_PlanetExpressLMSTest
             //make sure you got there
             var message = driver.FindElement(By.ClassName("page-title"));
             var value = message.Text;
-            Assert.AreEqual("CourseDetail", value);
+            Assert.AreEqual("Course Details", value);
         }
     }
 
@@ -263,18 +263,24 @@ namespace CS3750_PlanetExpressLMSTest
             var textboxPassword = driver.FindElement(By.Id("textbox-password"));
             var buttonLogin = driver.FindElement(By.Id("button-login"));
 
-            textboxUsername.SendKeys("fakestudent@mail.com");
+            textboxUsername.SendKeys("faketeacher@mail.com");
             textboxPassword.SendKeys("password");
             buttonLogin.Click();
+
+            //wait until logged in
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+            IWebElement element = wait.Until(ExpectedConditions.ElementIsVisible(By.Id("page-title")));
 
             //go to a details page
             var buttonDetails = driver.FindElement(By.ClassName("btn-primary"));
             buttonDetails.Click();
 
+
+
             //make sure you got there
             var message = driver.FindElement(By.ClassName("page-title"));
             var value = message.Text;
-            Assert.AreEqual("CourseDetail", value);
+            Assert.AreEqual("Course Details", value);
         }
     }
 
@@ -366,31 +372,6 @@ namespace CS3750_PlanetExpressLMSTest
             Assert.AreNotEqual(originalBalance, newBalance);
         }
 
-        [TestMethod]
-        public void ChromeCourseDetails()
-        {
-            //login
-            driver.Navigate().GoToUrl("https://localhost:44390/login");
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(500);
-
-            var textboxUsername = driver.FindElement(By.Id("textbox-username"));
-            var textboxPassword = driver.FindElement(By.Id("textbox-password"));
-            var buttonLogin = driver.FindElement(By.Id("button-login"));
-
-            textboxUsername.SendKeys("fakestudent@mail.com");
-            textboxPassword.SendKeys("password");
-            buttonLogin.Click();
-
-            //go to a details page
-            var buttonDetails = driver.FindElement(By.ClassName("btn-primary"));
-            buttonDetails.Click();
-
-            //make sure you got there
-            var message = driver.FindElement(By.ClassName("page-title"));
-            var value = message.Text;
-            Assert.AreEqual("CourseDetail", value);
-        }
-
 
         [TestMethod]
         public void EdgeCourseDetails()
@@ -407,7 +388,7 @@ namespace CS3750_PlanetExpressLMSTest
             var textboxPassword = driver.FindElement(By.Id("textbox-password"));
             var buttonLogin = driver.FindElement(By.Id("button-login"));
 
-            textboxUsername.SendKeys("test@test.com");
+            textboxUsername.SendKeys("faketeacher@mail.com");
             textboxPassword.SendKeys("password");
             buttonLogin.Click();
 
@@ -418,7 +399,7 @@ namespace CS3750_PlanetExpressLMSTest
             //make sure you got there
             var message = driver.FindElement(By.ClassName("page-title"));
             var value = message.Text;
-            Assert.AreEqual("CourseDetail", value);
+            Assert.AreEqual("Course Details", value);
         }
     }
 }
